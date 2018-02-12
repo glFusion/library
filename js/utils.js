19ed0b594bcb727cdccf950a646fca244ca69382
Lee Garner <lee@leegarner.com>
2018-02-12 10:18:16 -0800

    Enable ISBN lookup

--- js/utils.js
+++ js/utils.js
@@ -17,12 +17,64 @@ var LIBR_toggle = function(cbox, id, type, component) {
         success: function(result) {
             cbox.checked = result.newval == 1 ? true : false;
             try {
-                $.UIkit.notify("<i class='uk-icon-check'></i>&nbsp;" + result.statusMessage, {timeout: 1000,pos:'top-center'});
+                $.UIkit.notify("<i class='uk-icon uk-icon-check'></i>&nbsp;" + result.statusMessage, {timeout: 1000,pos:'top-center'});
             }
             catch(err) {
-                alert(result.statusMessage);
+                console.log(result.statusMessage);
             }
         }
     });
     return false;
 };
+
+/**
+*   Look up an ISBN from openlibrary.org
+*   Sets form field values from the lookup results.
+*
+*   @param  string  isbn    Item ISBN number
+*/
+var LIBR_openlibLookup = function(isbn) {
+    var key = "ISBN:" + isbn;
+    var dataS = {
+        "bibkeys" : key,
+        "jscmd" : "data",
+        "format": "json",
+    };
+    data = $.param(dataS);
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "https://openlibrary.org/api/books",
+        data: data,
+        success: function(result, textStatus, jqXHR) {
+            try {
+                var res = result[key];
+                LIBR_updateField(res.authors[0].name, "author");
+                LIBR_updateField(res.publishers[0].name, "publisher");
+                LIBR_updateField(res.title, "item_name");
+                LIBR_updateField(res.subtitle, "short_desc");
+            }
+            catch(err) {
+                console.log("openlibrary lookup error");
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log(errorThrown);
+        },
+    });
+    return false;
+};
+
+/**
+*   Update a form field from the library lookup results
+*
+*   @param  string  value   Value to set in field
+*   @param  string  elem_id Form element ID
+*/
+function LIBR_updateField(value, elem_id)
+{
+    if (typeof(value) != "undefined")
+        document.getElementById(elem_id).value = value;
+}
