5e6b1c45bc1d79b820a409a42c8a697df14e3b73
Lee Garner <lee@leegarner.com>
2020-07-28 10:04:37 -0700

    Update language strings

--- gettext.php
+++ gettext.php
@@ -3,7 +3,12 @@ namespace Library;
 
 class MO
 {
+    /** Language domain, e.g. plugin name.
+     * @var string */
     private static $domain = NULL;
+
+    /** Supported language name=>locale mapping.
+     * @var array */
     private static $lang2locale = array(
         'dutch' => 'nl',
         'finnish' => 'fi',
@@ -18,27 +23,39 @@ class MO
 
     /**
      * Initialize a language.
-     * Pass
+     * Sets the language domain and checks the requested language
      *
      * @access  public  so that notifications may set the language as needed.
-     * @param   string  $locale     Locale to use, global config by default
+     * @param   string  $lang   Language name, default is set by lib-common.php
      */
     public static function init($lang = NULL)
     {
         global $_CONF, $LANG_LOCALE;
 
-        self::$domain = 'library';
+        // Set the language domain to separate strings from the global
+        // namespace.
+        self::$domain = Config::getInstance()->get('pi_name');
+
+        // Set the requested language, falling back to the default if not
+        // specified.
         if ($lang === NULL) {
             $lang = $_CONF['language'];
         }
+
+        // Validate and use the appropriate locale code.
+        // Defaults to 'en_US' if a supportated locale wasn't requested.
         $parts = explode('_', $lang);
-        if (count($parts) > 2 && isset(self::$lang2local[$parts[0] . '_' . $parts[1]])) {
+        if (
+            count($parts) > 2 &&
+            isset(self::$lang2local[$parts[0] . '_' . $parts[1]])
+        ) {
             // 2-part language, e.g. "french_canada"
+            // Ignore any other parts like 'utf-8'
             $locale = self::$lang2locale[$parts[0] . '_' . $parts[1]];
         } elseif (isset(self::$lang2locale[$parts[0]])) {
             // single-part language, e.g. "english"
             $locale = self::$lang2locale[$parts[0]];
-        } elseif (!empty($LANG_LOCALE)) {
+        } elseif (isset($LANG_LOCALE) && !empty($LANG_LOCALE)) {
             // Not found, try the global variable
             $locale = $LANG_LOCALE;
         } else {
@@ -53,15 +70,39 @@ class MO
         }
     }
 
+
+    /**
+     * Get a singular or plural language string as needed.
+     *
+     * @param   string  $single     Singular language string
+     * @param   string  $plural     Plural language string
+     * @return  string      Appropriate language string
+     */
     public static function dngettext($single, $plural, $number)
     {
         if (!self::$domain) self::init();
         return \dngettext(self::$domain, $single, $plural, $number);
     }
+    public static function _n($single, $plural, $number)
+    {
+        return self::dngettext($single, $plural, $number);
+    }
+
+
+    /**
+     * Get a normal, singular language string.
+     *
+     * @param   string  $txt        Text string
+     * @return  string      Translated text
+     */
     public static function dgettext($txt)
     {
         if (!self::$domain) self::init();
         return \dgettext(self::$domain, $txt);
+    }
+    public static function _($txt)
+    {
+        return self::dgettext($text);
     }
 
 }
